71faca18fee11ab1eb5e73e464259a51afca5e33
Tyler Weeks <TylerW@mossadams.ma>
2020-03-18 15:10:24 -0700

    Add project files.

---
+++ DbUpDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DbUpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
